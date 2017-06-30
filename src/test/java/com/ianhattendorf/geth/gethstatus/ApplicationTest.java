e76da918f29fd8e8779c0c63adc217fbe7068101
Ian Hattendorf <ianhattendorf@gmail.com>
2017-06-30 12:46:05 -0700

    Test index should render

--- src/test/java/com/ianhattendorf/geth/gethstatus/ApplicationTest.java
+++ src/test/java/com/ianhattendorf/geth/gethstatus/ApplicationTest.java
@@ -2,15 +2,31 @@ package com.ianhattendorf.geth.gethstatus;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ApplicationTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
 
     @Test
     public void contextLoads() throws Exception {
     }
 
+    @Test
+    public void indexShouldRender() throws Exception {
+        assertThat(restTemplate.getForObject("http://localhost:" + port + "/", String.class))
+                .contains("Hello World!");
+    }
 }
