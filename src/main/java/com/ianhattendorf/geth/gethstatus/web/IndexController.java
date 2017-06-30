d9c27dccb065a4df3d6710bc57f0717bcb672baf
Ian Hattendorf <ianhattendorf@gmail.com>
2017-06-30 13:02:50 -0700

    Use Thymeleaf

--- src/main/java/com/ianhattendorf/geth/gethstatus/web/IndexController.java
+++ src/main/java/com/ianhattendorf/geth/gethstatus/web/IndexController.java
@@ -2,14 +2,12 @@ package com.ianhattendorf.geth.gethstatus.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
 
     @RequestMapping("/")
-    @ResponseBody
     public String index() {
-        return "Hello World!";
+        return "index";
     }
 }
