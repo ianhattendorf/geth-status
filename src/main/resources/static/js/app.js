99be314f1039d122d08b0325a8980f061e7416dd
Ian Hattendorf <ianhattendorf@gmail.com>
2017-07-01 18:31:32 -0700

    STOMP reconnect on failure

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,21 +1,35 @@
 if (window.WebSocket) {
+    stompConnect()
+}
+
+function stompConnect() {
     const wsUrl = `ws://${window.location.host}${contextRoot}/ws`
     console.log('wsUrl: ', wsUrl)
 
     const client = webstomp.client(wsUrl)
 
     client.connect({}, () => {
-        client.subscribe('/topic/status', message => {
-            const data = JSON.parse(message.body)
-            console.log("received: ", message, message.body, data)
-            // TODO view framework
-            document.getElementById('protocol-version').innerText = data.protocolVersion
-            document.getElementById('listening').innerText = data.listening
-            document.getElementById('peer-count').innerText = data.peerCount
-            document.getElementById('syncing').innerText = data.syncing
-            document.getElementById('block-number').innerText = data.blockNumber
-            const now = new Date()
-            document.getElementById('last-updated').innerText = moment().format('DD MMM YYYY HH:mm:ss')
-        })
-    })
+        document.getElementById('ws-status').innerText = 'Online'
+        client.subscribe('/topic/status', onStatusMessageReceived)
+    }, onStompError)
+}
+
+function onStatusMessageReceived(message) {
+    const data = JSON.parse(message.body)
+    console.log("received: ", message, message.body, data)
+    // TODO view framework
+    document.getElementById('protocol-version').innerText = data.protocolVersion
+    document.getElementById('listening').innerText = data.listening
+    document.getElementById('peer-count').innerText = data.peerCount
+    document.getElementById('syncing').innerText = data.syncing
+    document.getElementById('block-number').innerText = data.blockNumber
+    const now = new Date()
+    document.getElementById('last-updated').innerText = moment().format('DD MMM YYYY HH:mm:ss')
+}
+
+function onStompError(error) {
+    document.getElementById('ws-status').innerText = 'Offline'
+    console.log('STOMP: ' + error);
+    setTimeout(stompConnect, 10000);
+    console.log('STOMP: Reconnecting in 10 seconds...');
 }
