88bc44514ba38d39cf4b22410d2520e8b199e312
Ian Hattendorf <ianhattendorf@gmail.com>
2017-07-14 23:55:34 -0700

    Get correct uptime when up less than one hour
    Hour unit is optional in ps command

--- src/main/java/com/ianhattendorf/geth/gethstatus/service/LinuxShellGethUptimeService.java
+++ src/main/java/com/ianhattendorf/geth/gethstatus/service/LinuxShellGethUptimeService.java
@@ -20,7 +20,7 @@ public class LinuxShellGethUptimeService implements GethUptimeService {
 
     private static final Logger logger = LoggerFactory.getLogger(LinuxShellGethUptimeService.class);
 
-    private static final Pattern pattern = Pattern.compile("geth\\W*(?:(?<days>\\d+)-)?(?<hours>\\d{2}):(?<minutes>\\d{2}):(?<seconds>\\d{2})");
+    private static final Pattern pattern = Pattern.compile("geth\\W*(?:(?<days>\\d+)-)?(?:(?<hours>\\d{2}):)?(?<minutes>\\d{2}):(?<seconds>\\d{2})");
 
     @CacheResult(cacheName = "gethUptime")
     @Override
@@ -50,7 +50,9 @@ public class LinuxShellGethUptimeService implements GethUptimeService {
             if (days != null) {
                 instant = instant.minus(Duration.ofDays(Integer.valueOf(days)));
             }
-            instant = instant.minus(Duration.ofHours(Integer.valueOf(hours)));
+            if (hours != null) {
+                instant = instant.minus(Duration.ofHours(Integer.valueOf(hours)));
+            }
             instant = instant.minus(Duration.ofMinutes(Integer.valueOf(minutes)));
             instant = instant.minus(Duration.ofSeconds(Integer.valueOf(seconds)));
             // truncate to seconds to avoid appearing as a different time if off by a few milliseconds
